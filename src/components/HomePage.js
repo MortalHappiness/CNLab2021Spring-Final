3a025af194fad1b421688589d4ee2ddc5f2a662f
MortalHappiness <b07901069@ntu.edu.tw>
2021-06-06 20:39:53 +0800

    Finish HomePage

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,18 +1,44 @@
 import React from "react";
+import { Link, useHistory } from "react-router-dom";
 
-import { Button } from "@material-ui/core";
+import { SERVER_URL } from "../constants.json";
+
+import Button from "@material-ui/core/Button";
+
+function randint(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 export default function HomePage() {
+  const history = useHistory();
+
+  const startNow = () => {
+    fetch(`${SERVER_URL}/api/game/tours`)
+      .then((res) => res.json())
+      .then((json) => {
+        const tourID = randint(1, json.data);
+        history.push(`/Tour/${tourID}`);
+      })
+      .catch((e) => console.error(e));
+  };
+
   return (
     <div>
       <h1>Million $singer</h1>
-      <Button variant="contained" color="primary">
+      <Button variant="contained" color="primary" onClick={startNow}>
         Start Now
       </Button>
-      <Button variant="contained" color="primary">
+      <Button
+        variant="contained"
+        color="primary"
+        component={Link}
+        to="/TourSelect"
+      >
         Select Tournament
       </Button>
-      <Button variant="contained" color="primary">
+      <Button variant="contained" color="primary" component={Link} to="/Edit">
         Edit Your Game
       </Button>
     </div>
