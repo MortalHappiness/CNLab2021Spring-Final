c6533d4390966f596f341e6b46daa27a2d37eacc
MortalHappiness <b07901069@ntu.edu.tw>
2021-06-09 16:25:17 +0800

    Move images to images folder

--- src/components/utils.js
+++ src/components/utils.js
@@ -1,25 +1,25 @@
-import { makeStyles } from '@material-ui/core/styles';
-import img from "./bg.jpg";
-import img_light from "./bg_light.jpg";
-import img_dark from "./bg_dark.jpg";
+import { makeStyles } from "@material-ui/core/styles";
+import img from "../images/bg.jpg";
+import img_light from "../images/bg_light.jpg";
+import img_dark from "../images/bg_dark.jpg";
 
 export const button_styles = makeStyles({
   blue: {
-    background: '#a994f0',
+    background: "#a994f0",
     border: 0,
     borderRadius: 6,
-    color: '#white',
+    color: "#white",
     height: 48,
-    padding: '0 30px',
+    padding: "0 30px",
     margin: 32,
     fontSize: 30,
-    fontFamily: 'monospace',
+    fontFamily: "monospace",
   },
   white: {
-    fontFamily: 'monospace',
+    fontFamily: "monospace",
     fontSize: 20,
-    color: 'white',
-  }
+    color: "white",
+  },
 });
 
 export const bar_styles = makeStyles((theme) => ({
@@ -28,59 +28,59 @@ export const bar_styles = makeStyles((theme) => ({
     color: "#283747",
   },
   loginbutton: {
-    marginLeft: 'auto',
+    marginLeft: "auto",
   },
   menubutton: {
-    color: 'white'
-  }
+    color: "white",
+  },
 }));
 
 export const typo_styles = makeStyles((theme) => ({
   header: {
     padding: theme.spacing(1),
 
-    color: 'white',
+    color: "white",
     fontSize: 150,
-    fontFamily: 'DejaVu Sans Mono, monospace',
-    fontStyle: 'normal',
+    fontFamily: "DejaVu Sans Mono, monospace",
+    fontStyle: "normal",
   },
   sign: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    color: 'white',
+    alignItems: "center",
+    justifyContent: "center",
+    color: "white",
     fontSize: 20,
-    fontFamily: 'DejaVu Sans Mono, monospace',
-    fontStyle: 'normal',
+    fontFamily: "DejaVu Sans Mono, monospace",
+    fontStyle: "normal",
   },
   subheader: {
     padding: theme.spacing(1),
 
-    color: 'white',
+    color: "white",
     fontSize: 60,
-    fontFamily: 'DejaVu Sans Mono, monospace',
-    fontStyle: 'normal',
+    fontFamily: "DejaVu Sans Mono, monospace",
+    fontStyle: "normal",
   },
   songheader: {
     padding: theme.spacing(1),
 
-    color: 'white',
+    color: "white",
     fontSize: 50,
-    fontFamily: 'DejaVu Sans Mono, monospace',
-    fontStyle: 'normal',
-  }
+    fontFamily: "DejaVu Sans Mono, monospace",
+    fontStyle: "normal",
+  },
 }));
 
 export const background_styles = makeStyles((theme) => ({
   main: {
     backgroundImage: `url(${img})`,
-    width: '100%',
+    width: "100%",
   },
   light: {
     backgroundImage: `url(${img_light})`,
-    width: '100%',
+    width: "100%",
   },
   dark: {
     backgroundImage: `url(${img_dark})`,
-    width: '100%',
+    width: "100%",
   },
 }));
