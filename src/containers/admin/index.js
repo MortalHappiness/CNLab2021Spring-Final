b99ade688970546c859524d9b50c44244d428526
TPH <b07901087@ntu.edu.tw>
2021-06-13 11:48:03 +0800

    update background and title for edit game

--- src/containers/admin/index.js
+++ src/containers/admin/index.js
@@ -6,9 +6,13 @@ import Button from "@material-ui/core/Button";
 import Container from "@material-ui/core/Container";
 import Typography from "@material-ui/core/Typography";
 
+import img_dark from "../../images/bg_dark.jpg";
+import Box from "@material-ui/core/Box";
+
 const useStyles = makeStyles((theme) => ({
   paper: {
-    marginTop: theme.spacing(4),
+    marginTop: theme.spacing(0),
+	paddingTop: theme.spacing(4),
     display: "flex",
     flexDirection: "column",
     alignItems: "center",
@@ -16,15 +20,32 @@ const useStyles = makeStyles((theme) => ({
   button: {
     margin: theme.spacing(3, 0, 2),
   },
+  BackGround: {
+	backgroundImage: `url(${img_dark})`,
+    Width: "100%",
+	height: "95vh",  
+	paddingTop: "0%",
+	marginTop: "0%",
+  },
+  title: {
+	padding: theme.spacing(1),
+	color: "#FFFFFF",
+	fontSize: "5em",
+    fontFamily: "DejaVu Sans Mono, monospace",
+    fontStyle: "normal",
+  },
 }));
+
+
 
 export default function Admin() {
   const classes = useStyles();
 
   return (
+  <Box className={classes.BackGround}>
     <Container component="main" maxWidth="xs">
       <div className={classes.paper}>
-        <Typography component="h1" variant="h4">
+        <Typography component="h1" variant="h4" className={classes.title}>
           Edit Game
         </Typography>
         <Button
@@ -89,5 +110,6 @@ export default function Admin() {
         </Button>
       </div>
     </Container>
+  </Box>
   );
 }
